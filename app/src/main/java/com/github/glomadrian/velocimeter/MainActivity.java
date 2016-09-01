e9f793c342c56a59a25385a498d49e659173c9c5
newskysecurity <ls@newskysecurity.com>
2016-09-01 11:25:39 -0700

    Use timer to update UI

--- app/src/main/java/com/github/glomadrian/velocimeter/MainActivity.java
+++ app/src/main/java/com/github/glomadrian/velocimeter/MainActivity.java
@@ -1,12 +1,25 @@
 package com.github.glomadrian.velocimeter;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.SeekBar;
 import com.github.glomadrian.velocimeterlibrary.VelocimeterView;
 
 public class MainActivity extends AppCompatActivity {
 
+  //runs without a timer by reposting this handler at the end of the runnable
+  Handler timerHandler = new Handler();
+  Runnable timerRunnable = new Runnable() {
+
+    @Override
+    public void run() {
+      velocimeter.setValue((speed * 1.6 > 100)?100:speed * 1.6f, true);
+      velocimeter2.setValue(speed, true);
+
+      timerHandler.postDelayed(this, 500);
+    }
+  };
   private SeekBar seek;
   private VelocimeterView velocimeter;
   private VelocimeterView velocimeter2;
@@ -25,13 +38,14 @@ public class MainActivity extends AppCompatActivity {
     velocimeter = (VelocimeterView) findViewById(R.id.velocimeter);
     velocimeter2 = (VelocimeterView) findViewById(R.id.velocimeter2);
     handler = new SocketHandler("{'auth':'AuthMagic', 'subscribe':'root.engine.speed'}", this);
+    timerHandler.postDelayed(timerRunnable, 0);
   }
 
   private class SeekListener implements SeekBar.OnSeekBarChangeListener {
 
     @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-      velocimeter.setValue(speed, true);
-      velocimeter2.setValue(speed, true);
+//      velocimeter.setValue(speed, true);
+//      velocimeter2.setValue(speed, true);
     }
 
     @Override public void onStartTrackingTouch(SeekBar seekBar) {
